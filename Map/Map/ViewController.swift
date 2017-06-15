0d48dd54c407c02b238a9d8477a0aa0d3282025c
marcin <mwloczko@gmail.com>
2017-06-15 18:22:54 +0200

    Initialize project

---
+++ Map/Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by Marcin on 15.06.2017.
+//  Copyright © 2017 Marcin Włoczko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
