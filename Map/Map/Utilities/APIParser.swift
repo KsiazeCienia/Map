54b84ac679c59323bb108f7071623375b36adaab
marcin <mwloczko@gmail.com>
2017-06-16 10:21:08 +0200

    make parser to serwer response

--- Map/Map/Utilities/APIParser.swift
+++ Map/Map/Utilities/APIParser.swift
@@ -14,7 +14,16 @@ final class APIParser {
         var pins = [Pin]()
         
         for pinDict in response {
+            let id = pinDict[APIKeys.id] as? Int ?? 0
+            let name = pinDict[APIKeys.name] as? String ?? ""
+            let lat = pinDict[APIKeys.lat] as? Float ?? 0
+            let lng = pinDict[APIKeys.lng] as? Float ?? 0
+            let imagePath = pinDict[APIKeys.avatar] as? String ?? ""
             
+            let pin = Pin(id: id, lng: lng, lat: lat, name: name, imagePath: imagePath)
+            pins.append(pin)
         }
+        
+        return pins
     }
 }
