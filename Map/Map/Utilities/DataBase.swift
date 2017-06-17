d7e06855dbb89c21c7112e896e53a8d0d0f66a0e
marcin <mwloczko@gmail.com>
2017-06-17 17:06:40 +0200

    imporve database with save function

--- Map/Map/Utilities/DataBase.swift
+++ Map/Map/Utilities/DataBase.swift
@@ -11,14 +11,39 @@ import RealmSwift
 
 class DataBase {
     
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     func save(pin: Pin) {
+        let place = Place(context: context) // Link Task & Context
+        place.name = pin.name
+        place.id = Int64(pin.id)
+        place.lat = pin.lat
+        place.lng = pin.lng
+        place.imagePath = pin.imagePath
+        
+        // Save the data to coredata
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        //let _ = navigationController?.popViewController(animated: true)
+    }
+    
+    func getData() {
+        var places = [Place]()
+        
         do {
-            let realm = try Realm()
-            try realm.write {
-                //realm.add(pin)
-            }
-        } catch let error as NSError {
-            fatalError(error.localizedDescription)
+            places = try context.fetch(Place.fetchRequest())
+        } catch {
+            print("Fetching Failed")
+        }
+        
+        
+    }
+    
+    private func parsePlace(places: [Place]) {
+        var pins = [Pin]()
+        
+        for place in places {
+            //let pin = Pin(
         }
     }
 }
